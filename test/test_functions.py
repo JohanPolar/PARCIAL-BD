db25229542bdc450b0651ca6fb4d6eebd35e9b12
johanpolar <johanmoreno3003@gmail.com>
2023-03-12 19:00:24 -0500

    Arreglos flake8

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,11 +1,14 @@
 from funtions import downloadHTML, isntnullHTML, rqt_Mitula
-import pytest
-from unittest import mock
+
 
 def test_downloadHTML():
-	assert downloadHTML() == True
+    assert downloadHTML() is True
+
+
 def test_isntnullHTML():
-	assert isntnullHTML() == True
+    assert isntnullHTML() is True
+
+
 def test_rqt_Mitula(mocker):
     url = "https://casas.mitula.com.co/searchRE/nivel1-Cundinamarca/nivel2-Bogot%C3%A1/nivel3-Chapinero/orden-0/op-1/m2_min-1/m2_max-200/hab_min-1/ban_min-1/q-bogot%C3%A1?req_sgmt=REVTS1RPUDtVU0VSX1NFQVJDSDtTRVJQOw=="
     mocker.patch("requests.post", return_value=True)
